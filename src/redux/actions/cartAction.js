08ac37a2585c51fa5acef4a71c32fd502d22fd3b
Fatih <fatihce57@gmail.com>
2021-09-26 23:12:43 +0200

    remove cart action added

--- src/redux/actions/cartAction.js
+++ src/redux/actions/cartAction.js
@@ -2,3 +2,8 @@ export const addToCart = book => {
     return { type: 'ADD_TO_CART', payload: book }
 };
 
+export const removeFromCart = cartId => {
+    return { type: 'REMOVE_FROM_CART', payload: cartId }
+};
+
+
